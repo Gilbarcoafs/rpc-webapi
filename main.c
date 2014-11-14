8bb01e28da842589ba57269f6f0c2caefec02495
Martin <mdk@amescon.com>
2014-11-14 19:49:00 +0100

    wip

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include "mongoose.h"
 #include "rpc+.h"
 
@@ -25,14 +26,11 @@ static int get_query_param(const char *param, const char *query, char *value, in
   int param_length = strlen(param);
   /* is it a valid query string? */
 
-  printf("query: %s\n", query);
-
   if (query != NULL)
   {
     /* search within the query string */
     while (query[i] != '\0')
     {
-      printf("i: %i\n",i);
       /* if the query starts with the parameter */
       if (i == 0 || query[i-1] == '&')
         if(strncmp(query+i, param, param_length) == 0)
@@ -60,32 +58,77 @@ static int get_query_param(const char *param, const char *query, char *value, in
 
 static int ev_handler(struct mg_connection *conn, enum mg_event ev)
 {
-  char param1[255], param2[255];
-  switch (ev) {
+  char param_gpio[10], param_value[10], param_direction[10];
+  int gpio; int value = -1; bool input_value; gpio_direction_t direction;
+
+  
+  switch (ev)
+  {
     case MG_REQUEST:
+      if (get_query_param("gpio", conn->query_string, param_gpio, sizeof(param_gpio)) > 0)
+        gpio = atoi(param_gpio);
+      if (get_query_param("value", conn->query_string, param_value, sizeof(param_value)) > 0)
+        value = atoi(param_value);
+      get_query_param("direction", conn->query_string, param_direction, sizeof(param_direction));
+
+      printf("parameters: gpio=%i, value=%i, direction=%s\n", gpio, value, param_direction);
+
       if (is_request(uri_gpio_get))
         printf("gpio get\n");
       else if (is_request(uri_gpio_export))
       {
-        printf("gpio export\n");
-        if (get_query_param("gpio", conn->query_string, param1, sizeof(param1)) > 0)
-          printf("get_query_param('gpio') succeeded: %s\n", param1);
-        else
-          printf("get_query_param('gpio') failed.\n");
+        gpio_export(gpio);
+        mg_send_data(conn, NULL, 0);
       }
       else if (is_request(uri_gpio_unexport))
-        printf("gpio unexport\n");
+      {
+        gpio_unexport(gpio);
+        mg_send_data(conn, NULL, 0);
+      }
       else if (is_request(uri_gpio_value_get))
-        printf("gpio value get\n");
+      {
+        gpio_get_value(gpio, &input_value);
+        mg_send_data(conn, input_value ? "1" : "0", 1);
+      }
       else if (is_request(uri_gpio_value_set))
-        printf("gpio value set\n");
-      else if (is_request(uri_gpio_direction_get))
-        printf("gpio direction get\n");
-      else if (is_request(uri_gpio_direction_set))
-        printf("gpio direction set\n");
+      {
+        if (value == 0)
+          gpio_set_value(gpio, false);
+        else if (value == 1)
+          gpio_set_value(gpio, true);
 
-      /* */
-      mg_send_data(conn, NULL, 0);
+        mg_send_data(conn, NULL, 0);
+      }
+      else if (is_request(uri_gpio_direction_get))
+      {
+        if (gpio_get_direction(gpio, &direction))
+        {
+          switch (direction)
+          {
+            case gpio_direction_in:
+              mg_send_data(conn, "in", 2);
+              break;
+            case gpio_direction_out:
+              mg_send_data(conn, "out", 3);
+              break;
+          }
+        }
+        else
+          mg_send_data(conn, NULL, 0);
+
+      }
+      else if (is_request(uri_gpio_direction_set))
+      {
+        //printf("gpio_direction_set gpio=%i, direction=%s\n", gpio, param_direction);
+        if (strncmp("in", param_direction, 2) == 0)
+          gpio_set_direction(gpio, gpio_direction_in);
+        else if (strncmp("out", param_direction, 3) == 0)
+          gpio_set_direction(gpio, gpio_direction_out);
+
+        mg_send_data(conn, NULL, 0);
+      }
+      else
+        mg_send_data(conn, NULL, 0);
       return MG_TRUE;
       //return MG_MORE;
       break;
@@ -109,10 +152,45 @@ static int ev_handler(struct mg_connection *conn, enum mg_event ev)
     //  //return MG_MORE; // It is important to return MG_MORE after mg_send_file!
     //  return MG_TRUE;
 
+#define MAX_GPIOS 255
 
-int main(void) {
+void rpcp_test(void)
+{
+  int gpio_count;
+  gpio_get_gpio_count(&gpio_count);
+  printf("gpio_count: %i\n", gpio_count);
+
+  int gpios[MAX_GPIOS];
+  int i;
+  gpio_count = gpio_get_gpios(gpios, MAX_GPIOS);
+  printf("gpio_count: %i\n", gpio_count);
+  for (i = 0; i < MAX_GPIOS && i < gpio_count; i++)
+    printf("gpio %i: %i\n", i, gpios[i]);
+
+  printf("gpio_export(55): %i\n", gpio_export(55));
+
+  gpio_direction_t direction;
+  if (gpio_get_direction(55, &direction))
+    printf("gpio_get_direction(55) was successful: %i\n", direction);
+  else
+    printf("gpio_get_direction(55) failed\n");
+
+  bool value;
+  if (gpio_get_value(55, &value))
+    printf("gpio_get_value(55) was successful: %i\n", value);
+  else
+    printf("gpio_get_value(55) failed\n");
+
+  printf("gpio_unexport(55): %i\n", gpio_unexport(55));
+
+}
+
+int main(void)
+{
   struct mg_server *server = mg_create_server(NULL, ev_handler);
   mg_set_option(server, "listening_port", "8080");
+
+  //rpcp_test();
 
   printf("Starting rpc+webapi on port %s\n", mg_get_option(server, "listening_port"));
   for (;;) mg_poll_server(server, 1000);
