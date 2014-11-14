cbc2b4052f71c7bc7dc7188037e735ccdd7394d8
Martin <mdk@amescon.com>
2014-11-14 15:17:53 +0100

    rpc+.c implements gpio_get_gpio_count()

--- rpc+.c
+++ rpc+.c
@@ -1,11 +1,60 @@
-#include "rpc+.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 #include <dirent.h>
+#include "rpc+.h"
 
-//bool gpio_get_gpio_count(int *gpio_count)
-//{
+#define dir_gpio "/sys/class/gpio"
+#define dir_name_gpiochip "gpiochip"
+#define dir_name_ngpio "ngpio"
+
+bool gpio_get_gpio_count(int *gpio_count)
+{
   /* get the number of all gpios by enumerating all gpio chips */
-  //DIR *dir = opendir("/sys/class/gpio");
-//}
+  DIR *dir;
+  struct dirent *entry;
+  FILE* file;
+  int bytes_read_count, i, count = 0;
+  char filepath[255], buffer[255];
+
+  memset(buffer, 0, sizeof(buffer));
+
+  /* open the gpio directory */
+  dir = opendir(dir_gpio);
+
+  if (dir != NULL)
+  {
+    while((entry = readdir(dir)) != NULL)
+    {
+      //printf("entry->d_name: %s\n", entry->d_name);
+      /* look for gpiochip directories... */
+      if (strncmp(entry->d_name, dir_name_gpiochip, strlen(dir_name_gpiochip)) == 0)
+      {
+        /* try to read their ngpio file */
+        snprintf(filepath, sizeof(filepath), "%s/%s/%s", dir_gpio, entry->d_name, dir_name_ngpio);
+        printf("ngpio: %s\n", filepath);
+        file = fopen(filepath, "r");
+        if (file != NULL)
+        {
+          /* read the file */
+          bytes_read_count = fread(buffer, sizeof(char), sizeof(buffer)-1, file);
+
+          if (bytes_read_count > 0)
+            count += atoi(buffer);
+
+          /* close the file */
+          fclose(file);
+        }
+      }
+    }
+
+    /* close the dir again */
+    closedir(dir);
+  }
+
+  *gpio_count = count;
+}
+
 //bool gpio_get_gpios(int* gpios, int max_count);
 //bool gpio_export(int gpio);
 //bool gpio_unexport(int gpio);
